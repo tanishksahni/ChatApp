9e654cca4548dd0a87bed4e9fdf8e85dd31c8d52
tanishksahni <tanishk.sahni2002@gmail.com>
2024-05-03 17:48:51 +0530

    setup

---
+++ ChatApp/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  ChatApp
+//
+//  Created by Tanishk Sahni on 03/05/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
